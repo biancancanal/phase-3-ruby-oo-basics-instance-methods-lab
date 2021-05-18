2d2bb25a21bd9ce62865c4acc4322c9449174457
Bianca <biancancanal@gmail.com>
2021-05-18 09:48:53 -0500

    Complete

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    # some code to describe a dog
+    def bark
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end
+end 
+
+fido = Dog.new
+fido.bark
+fido.sit
